d9f9f6641e31e50a88e7d956a3a843a98dd93225
Srikalyan Megati <kalmegati@gmail.com>
2019-10-21 19:59:01 +0000

    Done.

--- table_parser.rb
+++ table_parser.rb
@@ -9,24 +9,30 @@ def table_header(table)
   headers = []
   if head.css("th").length > 0
     head.css("th").each { |cell|
-      headers << cell.text
+      headers << cell.text.upcase
     }
   elsif head.css("td").length > 0
     head.css("td").each { |cell|
-      headers << cell.text
+      headers << cell.text.upcase
     }
   end
-  puts headers
-end
-
-def row_builder
-  
+  headers
 end
 
 def table_data(table)
   data = table.css("tr")
   data.shift
   daters = data.map{|row|row.css("td").map{|cell|cell.text.split(", ")}}
+end
+
+def table_parser(headers, daters)
+  head = "| "
+  headers.each { |categ|
+    head << categ
+    head << " | "
+  }
+  
+  puts head
   
   daters.each { |row|
     columns = row.max{ |a,b| a.length <=> b.length }.length
@@ -40,11 +46,12 @@ def table_data(table)
   
   daters.each { |row|
     #filled_row = "| "
-    row[1].each_with_index { |feature, index| 
+    row[0].each_with_index { |feature, index| 
       filled_row = "| "
-      filled_row << row[0][index]
-      filled_row << cell_break
-      filled_row << feature
+      row.length.times { |time|
+        filled_row << row[time][index]
+        filled_row << cell_break
+      }
       puts filled_row
       #filled_row << cell_break
       #puts filled_row
@@ -53,35 +60,12 @@ def table_data(table)
     puts row_break
   }
   
-  # daters.each { |row|
-    
-  #   full_row = "| "
-  #   full_row << row[0][0]
-  #   full_row << cell_break
-  #   full_row << row[1][0]
-  #   puts full_row
-    
-  #   full_row = "| "
-  #   full_row << row[0][1]
-  #   full_row << cell_break
-  #   full_row << row[1][1]   
-  #   puts full_row
-    
-  #   puts row_break
-  # }
-  
   #binding.pry
   #puts daters
 end
 
-#table_header(html)
+headers = table_header(html)
 
-table_data(html)
+daters = table_data(html)
 
-#binding.pry
-
-def table_parser(table)   #=> input post-nokogiri <table> selector
-  
-  
-  
-end
+table_parser(headers, daters)
