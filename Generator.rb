4dc6bfce866d8ea0a6dac11b4e6c3913a70e91b1
Srikalyan Megati <kalmegati@gmail.com>
2019-10-22 20:23:50 +0000

    Done.

--- Generator.rb
+++ Generator.rb
@@ -2,8 +2,34 @@ require 'pry'
 require 'open-uri'
 require 'nokogiri'
 
+source = "http://2e.aonprd.com/"
+
 html = Nokogiri::HTML(open('http://2e.aonprd.com/Ancestries.aspx'))
 
-info = html.css("h1").css("a").select{|info|!info.text.include?(" ")}
+races = html.css("h1").css("a").select{|info|!info.text.include?(" ")}
+
+info = html.css("div#main").css("span")[-1]
+
+info.css("h2").remove
+
+desc = info.text.split("Click here for more details.")
+
+racebreak = {}
+
+races.map{|race|race.text}.each_with_index{ |race, index|
+  racebreak[race] = desc[index]
+}
+
+# race = Nokogiri::HTML(open(source + info[0]["href"]))
+
+# race_heads = race.css("div.main").css("h2").map{|heading|heading.text}
+
+# flastar = race_heads.index("You Might...")
+
+# mechstar = race_heads.index("Hit Points")
+
+# flavor = race_heads[flastar...mechstar]
+
+# mechanics = race_heads[mechstar...]
 
 binding.pry
